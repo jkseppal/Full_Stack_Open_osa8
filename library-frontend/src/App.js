27cce3e9ea6aeedb29e02f03fbb905d0ec44c654
jkseppal <jani.k.seppala@gmail.com>
2020-09-11 09:15:22 +0300

    8.9 tehty

---
+++ library-frontend/src/App.js
@@ -0,0 +1,56 @@
+
+import React, { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+
+/*const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      id
+    }
+  }
+`
+
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      published
+      author
+      genres
+      id
+    }
+  }
+`*/
+
+const App = () => {
+  const [page, setPage] = useState('authors')
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors
+        show={page === 'authors'}
+      />
+
+      <Books
+        show={page === 'books'}
+      />
+
+      <NewBook
+        show={page === 'add'}
+      />
+
+    </div>
+  )
+}
+
+export default App
